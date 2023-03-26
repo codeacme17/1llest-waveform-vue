afcc4570f88a054e94498f2bb0f0fdc47d586eda
codeacme17 <vir_kings@qq.com>
2023-03-26 14:48:12 +0800

    perf: add `throttle` func to optimize scrolling event

--- src/utils/lazy-load.ts
+++ src/utils/lazy-load.ts
@@ -1,19 +1,17 @@
-/** Registers a scroll event listener on the document that checks if the given
-    element is in view and calls the provided handler if it is.
-*/
+// Registers a scroll event listener on the document that checks if the given
+// element is in view and calls the provided handler if it is.
 function registerScrollHander(el: HTMLElement, handler: () => void) {
-  document.addEventListener('scroll', () => lazyLoader(el, handler))
+  const throttledHandler = throttle(() => lazyLoader(el, handler), 500)
+  document.addEventListener('scroll', () => throttledHandler())
 }
 
-/** Removes the scroll event listener on the document that was previously added
-    with registerScrollHandler.
-*/
+// Removes the scroll event listener on the document that was previously added
+// with registerScrollHandler.
 function canelScrollHander(el: HTMLElement, handler: () => void) {
   document.removeEventListener('scroll', () => lazyLoader(el, handler))
 }
 
-/** Checks if the given element is in view, and if it is, calls the provided handler.
- */
+// Checks if the given element is in view, and if it is, calls the provided handler.
 function lazyLoader(el: HTMLElement, handler: () => void): void {
   const windowHeight: number = window.innerHeight
   const windowOffY: number = window.scrollY
@@ -25,6 +23,18 @@ function lazyLoader(el: HTMLElement, handler: () => void): void {
     elDistanceToTop - windowOffY - windowHeight < windowHeight / 4
   )
     handler()
+}
+
+// Throttle function that delays the execution of the given function until a certain time has passed since the last time it was called.
+function throttle(func: () => void, delay: number): () => void {
+  let timeout: NodeJS.Timeout | undefined
+  return () => {
+    if (!timeout)
+      timeout = setTimeout(() => {
+        func()
+        timeout = undefined
+      }, delay)
+  }
 }
 
 export { registerScrollHander, canelScrollHander, lazyLoader }
