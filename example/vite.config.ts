5e6cead8d22caadd74969f55946274d45761b5df
codeacme17 <vir_kings@qq.com>
2023-03-27 14:23:34 +0800

    chore: rebuild example dir

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,14 @@
 import path from "path"
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base:"/1llest-waveform-vue/",
+  build: {
+    outDir: resolve(__dirname,"../docs")
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
